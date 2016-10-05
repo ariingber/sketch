e7281e1c5f3f17a5eae96f227668389c80751c58
Ari Ingber <ariingber@gmail.com>
2016-10-05 12:05:26 -0400

    trying carousel on phone first time

--- public/js/init.js
+++ public/js/init.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     $(".button-collapse").sideNav();
     $('.materialboxed').materialbox();
     $('ul.tabs').tabs();
+    $('.carousel.carousel-slider').carousel({full_width: true});
     $('.modal-trigger').leanModal();
     $('.scrollspy').scrollSpy();
     $('.slider').slider({full_width: true, indicators: false, height: 200 });
