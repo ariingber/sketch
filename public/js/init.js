215df4daeb66c8207ed287837c033406acc1d3c9
Ari Ingber <ariingber@gmail.com>
2016-10-11 14:36:07 -0400

    joe test 3

--- public/js/init.js
+++ public/js/init.js
@@ -27,4 +27,8 @@ $(document).ready(function(){
       $('#health_care').hover( function() {
         $('#help_dropdown').fadeOut();
       });
+      $('#play_button_link').on('click', function(ev) {
+       $("#joe_video")[0].src += "&autoplay=1";
+       ev.preventDefault();
+     });
 });
