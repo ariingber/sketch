c0c142d9e86ea5384fd2345191021c878cc642f5
Ari Ingber <ariingber@gmail.com>
2016-11-21 13:47:48 -0500

    carosuel update

--- public/js/init.js
+++ public/js/init.js
@@ -63,7 +63,7 @@ $(document).ready(function(){
       // *****************************************************
 
     // build better habbits CAROUSEL for mobile ***************************************************
-    $('.carousel.carousel-slider#build_better_habbits_carousel').carousel({full_width: true});
+    $('.carousel.carousel-slider#build_better_habbits_carousel_ipad').carousel({full_width: true});
 
     // Build better habbits slider should go first mobile
     $('.slider.testimonial_slider').slider({indicators:true, height:150 });
