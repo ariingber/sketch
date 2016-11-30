3520621f869f67491c21c7ad54650409aa234224
Ari Ingber <ariingber@gmail.com>
2016-11-30 13:49:29 -0500

    multiple changes to form few bugs to work out

--- public/js/init.js
+++ public/js/init.js
@@ -230,7 +230,7 @@ $(document).ready(function(){
     $('#last_name_error').hide();
     $( "#thank_you_message" ).hide();
 
-
+// UNIQUE BUTTONS
     $("#choice_send_message").click(function (event) {
       $('#question1').animate({left: '150px'}, 'fast').fadeOut(300);
       $('#message_div').delay(800).fadeIn(300);
@@ -255,7 +255,6 @@ $(document).ready(function(){
         $('#question2').css({'left':'0'})
       }, 1000);
     });
-
     $("#start_over").click(function (event) {
       $('#sorry_message_div').animate({left: '150px'}, 'fast').fadeOut(300);
       $('#question1').delay(800).fadeIn(300);
@@ -263,96 +262,93 @@ $(document).ready(function(){
         $('#sorry_message_div').css({'left':'0'})
       }, 1000);
     });
-
-    $("#try_clinical_study").click(function (event) {
+    $("#will_try_clinical_study").click(function (event) {
       $('#question2').animate({left: '150px'}, 'fast').fadeOut(300);
       $('#question3').delay(800).fadeIn(300);
       setTimeout(function () {
         $('#first_name').focus();
         $('#question2').css({'left':'0'})
       }, 1000);
-      $('#progress_counter').delay(800).fadeIn(300);
+      count = 1;
+      $('#count').text(count + '');
+      $('#progress_counter').fadeOut('fast').delay(700).fadeIn(500);
     });
+////////////////////////////////////////////
 
-    var count = 1;
 
+
+    // $("#try_clinical_study").click(function (event) {
+    //   $('#question2').animate({left: '150px'}, 'fast').fadeOut(300);
+    //   $('#question3').delay(800).fadeIn(300);
+    //   setTimeout(function () {
+    //     $('#first_name').focus();
+    //     $('#question2').css({'left':'0'})
+    //   }, 1000);
+    //   $('#progress_counter').delay(800).fadeIn(300);
+    // });
+
+
+    var count = 0;
     function fire (currentInput) {
       count += 1;
-      currentInput.animate({height: 'toggle'}, 'slow').fadeOut(300);
-      currentInput.next().delay(700).animate({height: 'toggle'}, 'slow').fadeIn(500);
+      currentInput.animate({left: '150px'}, 'fast').fadeOut(300);
+      currentInput.next().delay(800).fadeIn(300);
       setTimeout(function () {
+        currentInput.css({'left':'0'})
         currentInput.next().find('input').focus()
       }, 1000);
        $('#count').text(count + '');
        $('#progress_counter').fadeOut('fast').delay(700).fadeIn(500);
+
     }
 
     function formEnterFunction(currentInput) {
-      switch (currentInput.find('input')[0].id){
-        case 'first_name':
-          if ($("#first_name").val() == '') {
-            $('#first_name_error').fadeIn().delay(900).fadeOut();
-          }
-          else {
-            fire(currentInput)
-          }
-          break;
-        case 'last_name':
-          if ($("#last_name").val() == '') {
-            $('#last_name_error').fadeIn().delay(900).fadeOut();
-          }
-          else {
-            fire(currentInput)
-          }
-          break;
-        case 'email':
-          var $email = $("#email").val();
-          var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
-          if ($email == '' || !re.test($email)) {
-            $('#email_error').fadeIn().delay(900).fadeOut();
-          }
-          else {
-            fire(currentInput)
-          }
-          break;
-        default :
-          fire(currentInput);
+      if (currentInput.find('input')[0] == undefined) {
+        fire(currentInput);
       }
-    }
+      else {
+        switch (currentInput.find('input')[0].id){
+          case 'first_name':
+            if ($("#first_name").val() == '') {
+              $('#first_name_error').fadeIn().delay(900).fadeOut();
+            }
+            else {
+              fire(currentInput)
+            }
+            break;
+          case 'last_name':
+            if ($("#last_name").val() == '') {
+              $('#last_name_error').fadeIn().delay(900).fadeOut();
+            }
+            else {
+              fire(currentInput)
+            }
+            break;
+          case 'email':
+            var $email = $("#email").val();
+            var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
+            if ($email == '' || !re.test($email)) {
+              $('#email_error').fadeIn().delay(900).fadeOut();
+            }
+            else {
+              fire(currentInput)
+            }
+            break;
+          default :
+            fire(currentInput);
+            }
+          }
+      }
+
 
     $('.nextButton').click(function(){
-      var currentInput = $(this).parent().parent();
+      var currentInput = $(this).parent();
       formEnterFunction(currentInput);
     });
 
 
-    // $("#question3 input").keypress(function (event) {
-    //   var currentInput = $(this).parent().parent();
-    //    if (event.which == 13) {
-    //      event.preventDefault();
-    //      formEnterFunction(currentInput);
-    //      event.stopPropagation();
-    //    }
-    // });
-    //
-    // $("#question4 input").keypress(function (event) {
-    //   var currentInput = $(this).parent().parent();
-    //    if (event.which == 13) {
-    //     event.preventDefault();
-    //      formEnterFunction(currentInput);
-    //    }
-    // });
-    //
-    // $("#question5 input").keypress(function (event) {
-    //   var currentInput = $(this).parent().parent();
-    //    if (event.which == 13) {
-    //      event.preventDefault();
-    //      formEnterFunction(currentInput);
-    //     }
-    //   });
-
     $(".question input").keypress(function (event) {
-      var currentInput = $(this).parent().parent();
+      var currentInput = $(this).parent();
        if (event.which == 13) {
          event.preventDefault();
          formEnterFunction(currentInput);
@@ -394,24 +390,29 @@ $(document).ready(function(){
     });
 
     function backButtonFunction(currentInput) {
+      currentInput.animate({right: '150px'}, 'fast').fadeOut(300);
+      currentInput.prev().delay(800).fadeIn(300);
+      setTimeout(function () {
+        currentInput.prev().find('input').focus()
+        currentInput.css({'right':'0'})
+        currentInput.css({'left':'0'})
+      }, 1000);
       count -= 1;
       if (count < 1) {
         count = 1;
+        $('#progress_counter').hide();
       }
-      currentInput.animate({height: 'toggle'}, 'slow').fadeOut(300);
-      currentInput.prev().delay(900).animate({height: 'toggle'}, 'slow').fadeIn(500);
-      setTimeout(function () {
-        currentInput.prev().find('input').focus()
-      }, 1000);
-      $('#count').text(count + '');
-      $('#progress_counter').fadeOut('fast').delay(700).fadeIn(500);
+      else {
+        $('#count').text(count + '');
+        $('#progress_counter').fadeOut('fast').delay(700).fadeIn(500);
+      }
     }
 
-    $("#backButton1").click(function () {
-      var currentInput = $(this).parent();
-      currentInput.animate({height: 'toggle'}, 'slow').fadeOut(300);
-      currentInput.prev().delay(900).animate({height: 'toggle'}, 'slow').fadeIn(500);
-    });
+    // $("#backButton1").click(function () {
+    //   var currentInput = $(this).parent();
+    //   currentInput.animate({right: '150px'}, 'fast').fadeOut(300);
+    //   currentInput.prev().delay(900).animate({height: 'toggle'}, 'slow').fadeIn(500);
+    // });
 
     $("#backButtonFirstName").click(function () {
       var currentInput = $(this).parent().parent();
@@ -425,31 +426,17 @@ $(document).ready(function(){
       backButtonFunction(currentInput);
     });
 
-    // $("#backButton4").click(function () {
-    //   var currentInput = $(this).parent().parent();
-    //   backButtonFunction(currentInput);
-    // });
-    // $("#backButton5").click(function () {
-    //   var currentInput = $(this).parent().parent();
-    //   backButtonFunction(currentInput);
-    // });
     $(".backButtonType2").click(function () {
       var currentInput = $(this).parent();
       backButtonFunction(currentInput);
     });
-    // $("#backButton7").click(function () {
-    //   var currentInput = $(this).parent();
-    //   backButtonFunction(currentInput);
-    // });
-
-    // $("#addtionalCommentsBackButton").click(function () {
-    //   var currentInput = $(this).parent();
-    //   backButtonFunction(currentInput);
-    // });
 
     $("#messageBackButton").click(function () {
-      $('#message_div').animate({height: 'toggle'}, 'slow').fadeOut(300);
-      $('#question1').delay(900).animate({height: 'toggle'}, 'slow').fadeIn(500);
+      $('#message_div').animate({right: '150px'}, 'fast').fadeOut(300);
+      $('#question1').delay(800).fadeIn(300);
+      setTimeout(function () {
+        $('#message_div').css({'right':'0'})
+      }, 1000);
     });
 
 
